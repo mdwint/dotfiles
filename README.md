1e6563b2d3ba2ca31bae44dfcbc8b6a60877841c
Matteo De Wint <matteo@novemberfive.co>
2019-08-08 14:27:30 +0200

    Secrets; add script to backup & restore secrets

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 ```
 curl https://raw.githubusercontent.com/mdwint/dotfiles/master/install.sh -sSf | sh
 ```
+
+## Managing secrets
+
+Backup secrets from your home directory to an encrypted archive:
+
+```
+~/dotfiles/secrets.sh backup secrets.tar.gz.encrypted .aws/ .ssh/
+```
+
+Restore archived secrets on a new machine:
+
+```
+~/dotfiles/secrets.sh restore secrets.tar.gz.encrypted
+```
---
+++ secrets.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+usage() {
+    echo "Usage: $0 <backup|restore> <archive-filename> [<source-path>...]" 1>&2
+    exit 1
+}
+
+[ $# -ge 2 ] || usage
+CIPHER=aes256
+COMMAND=$1
+ARCHIVE=$2
+shift 2
+
+case $COMMAND in
+backup)
+    tar -czf - -C "$HOME" "$@" | openssl enc -$CIPHER -e -out "$ARCHIVE"
+    ;;
+restore)
+    openssl enc -$CIPHER -d -in "$ARCHIVE" | tar -xzk -C "$HOME"
+    ;;
+*)
+    usage
+    ;;
+esac
