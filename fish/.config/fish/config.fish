0f742a64dc641ee6371e4e71439121b53198458d
Matteo De Wint <matteo@novemberfive.co>
2019-08-09 10:10:54 +0200

    git; add config & aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,6 @@ set fish_color_user normal
 set fish_color_cwd brblue
 set -U fish_prompt_pwd_dir_length 0
 
-function fish_right_prompt
-end
-
 set PATH ~/bin ~/.cargo/bin ~/.poetry/bin $PATH
 
 set -xU FZF_DEFAULT_COMMAND 'ag --nocolor --ignore node_modules -g ""'
@@ -14,6 +11,12 @@ set -xU MANPAGER 'most -wd'
 alias l 'exa -l'
 alias tree "exa -T -I='__pycache__|node_modules'"
 alias vim nvim
+alias gl 'git log'
+alias gg 'git log --all --graph --oneline --decorate'
+alias gs 'git status'
+alias gd 'git diff'
+alias gD 'git diff --staged'
+alias ga 'git add'
 
 direnv hook fish | source
 thefuck --alias | source
