915fc3c2e914033914cf17b059f4906e9a2369ea
Matteo De Wint <matteo@gorilla.co>
2021-07-21 21:57:42 +0200

    fish; improve git prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -2,7 +2,11 @@ function fish_prompt --description 'Write out the prompt'
     set_color $fish_color_cwd
     echo -n (prompt_pwd)
 
-    set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    set -l branch (
+        git describe --tags --exact-match 2> /dev/null ||
+        git symbolic-ref -q --short HEAD 2> /dev/null ||
+        git rev-parse --short HEAD 2> /dev/null
+    )
     if test -n "$branch"
         set_color $fish_color_escape
         echo -n " ($branch)"
