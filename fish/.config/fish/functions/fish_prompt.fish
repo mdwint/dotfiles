866f4c6a437bd6714d89cf8b970566b7b27875e0
Matteo De Wint <matteo@gorilla.co>
2021-10-25 17:16:00 +0200

    fish; show git tag after branch in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -3,11 +3,12 @@ function fish_prompt --description 'Write out the prompt'
     echo -n (prompt_pwd)
 
     set -l branch (
-        git describe --tags --exact-match 2> /dev/null ||
         git symbolic-ref -q --short HEAD 2> /dev/null ||
         git rev-parse --short HEAD 2> /dev/null
     )
     if test -n "$branch"
+        set -l tag (git describe --tags --exact-match 2> /dev/null)
+        if test -n "$tag"; set branch "$branch, $tag"; end
         set_color $fish_color_escape
         echo -n " ($branch)"
     end
