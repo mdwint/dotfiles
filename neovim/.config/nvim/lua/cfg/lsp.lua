c1d89d5fc502811601ad6f1b0b2f6ae6c3be5144
Matteo De Wint <matteo@gorilla.co>
2022-04-12 16:14:54 +0200

    nvim; update treesitter config

--- neovim/.config/nvim/lua/cfg/lsp.lua
+++ neovim/.config/nvim/lua/cfg/lsp.lua
@@ -68,6 +68,6 @@ for _, lsp in ipairs(servers) do
 end
 
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   highlight = { enable = true },
 })
