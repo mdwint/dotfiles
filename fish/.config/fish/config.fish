79c67a987384e40eeea21d98315161c60802a782
Matteo De Wint <matteo@gorilla.co>
2022-04-19 16:06:47 +0200

    fish; add fnm env

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,7 @@ end
 
 if status --is-interactive
     if type -q direnv; eval (direnv hook fish); end
+    if type -q fnm; eval (fnm env); end
     if type -q pyenv; pyenv init --path --no-rehash | source; end
     if type -q zoxide; zoxide init fish | source; end
 
