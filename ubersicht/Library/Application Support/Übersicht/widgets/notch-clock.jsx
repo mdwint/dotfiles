ce33da69f6b78bd7a2c3822ae3de3b9dfb1109dc
Matteo De Wint <matteo@gorilla.co>
2022-04-25 13:19:17 +0200

    ubersicht; add widgets

---
+++ ubersicht/Library/Application Support/Übersicht/widgets/notch-clock.jsx
@@ -0,0 +1,17 @@
+export const command = "date '+%a %d/%m %H:%M'";
+export const refreshFrequency = 1000;
+
+export const className =`
+  right: 8px;
+  top: 12px;
+
+  font-family: 'Iosevka Term Light';
+  font-size: 16;
+  color: white;
+`;
+
+export const render = ({output}) => {
+  return (
+    <div>{output}</div>
+  );
+};
---
+++ ubersicht/Library/Application Support/Übersicht/widgets/notch-window-title.jsx
@@ -0,0 +1,32 @@
+export const command = `osascript -e '
+global frontApp, frontAppName, windowTitle
+
+set windowTitle to ""
+tell application "System Events"
+    set frontApp to first application process whose frontmost is true
+    set frontAppName to name of frontApp
+    tell process frontAppName
+        tell (1st window whose value of attribute "AXMain" is true)
+            set windowTitle to value of attribute "AXTitle"
+        end tell
+    end tell
+end tell
+
+return {windowTitle}
+'`;
+export const refreshFrequency = 100;
+
+export const className =`
+  left: 8px;
+  top: 12px;
+
+  font-family: 'Iosevka Term Light';
+  font-size: 16;
+  color: white;
+`;
+
+export const render = ({output}) => {
+  return (
+    <div>{output}</div>
+  );
+};
