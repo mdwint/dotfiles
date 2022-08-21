56d802562388c57e215941e5be7f6811c5166550
Matteo De Wint <matteo@gorilla.co>
2022-08-21 13:19:55 +0200

    Add macos-defaults.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ fi
     [ -d "$base16_shell" ] || git clone https://github.com/chriskempson/base16-shell.git "$base16_shell"
 
     if [ "$os" = macos ]; then
+        ./macos-defaults.sh
         stow */
         tic -x tmux/.tmux-terminfo.src
         brew bundle --no-lock --no-upgrade
