9228d156c3543fb895787b84a7473f9b05c34cd9
Matteo De Wint <matteo@gorilla.co>
2023-07-14 11:09:14 +0200

    neovim: update keymaps

--- neovim/.config/nvim/lua/cfg/telescope.lua
+++ neovim/.config/nvim/lua/cfg/telescope.lua
@@ -19,9 +19,11 @@ require("telescope").setup({
 require("telescope").load_extension("fzf")
 
 vim.cmd([[
-nnoremap <c-p> :Telescope find_files hidden=true theme=get_ivy<cr>
-nnoremap <leader>a :Telescope live_grep theme=get_ivy<cr>
-nnoremap <leader>8 :Telescope grep_string theme=get_ivy<cr>
-nnoremap <leader>b :Telescope buffers theme=get_ivy<cr>
-nnoremap <leader>h :Telescope help_tags theme=get_ivy<cr>
+nnoremap <c-p> :Telescope find_files hidden=true theme=ivy<cr>
+nnoremap <leader>a :Telescope live_grep theme=ivy<cr>
+nnoremap <leader>8 :Telescope grep_string theme=ivy<cr>
+nnoremap <leader>b :Telescope buffers theme=ivy<cr>
+nnoremap <leader>h :Telescope help_tags theme=ivy<cr>
+nnoremap <leader>l :Telescope loclist theme=ivy<cr>
+nnoremap <leader>m :Telescope keymaps theme=ivy<cr>
 ]])
