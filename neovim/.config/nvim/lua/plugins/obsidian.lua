f0648a7b8269577a2a52a1881f0c3e0bd513aba0
Matteo De Wint <matteo@gorilla.co>
2023-07-16 00:11:13 +0200

    neovim: remove obsidian plugin

--- neovim/.config/nvim/lua/plugins/obsidian.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "epwalsh/obsidian.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  lazy = true,
-  event = { "BufReadPre " .. vim.fn.expand("~") .. "/notes/**.md" },
-  opts = { dir = "~/notes" },
-}
