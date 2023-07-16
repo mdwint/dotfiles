6aa11890f14e701ebe84653366e4b09074d7be6b
Matteo De Wint <matteo@gorilla.co>
2023-07-16 12:11:26 +0200

    blink: add theme

---
+++ blink/themes/mdwint-dark.js
@@ -0,0 +1,21 @@
+t.prefs_.set("color-palette-overrides", [
+  "#101010",
+  "#974b46", // red
+  "#5f8787", // green
+  "#a79d78", // yellow
+  "#686868",
+  "#747474",
+  "#868686",
+  "#b9b9b9",
+  "#525252",
+  "#974b46", // red
+  "#5f8787", // green
+  "#a79d78", // yellow
+  "#686868",
+  "#747474",
+  "#868686",
+  "#f7f7f7",
+]);
+t.prefs_.set("foreground-color", "#b9b9b9");
+t.prefs_.set("background-color", "#000000");
+t.prefs_.set("cursor-color", "#b9b9b9");
