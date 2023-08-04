b6598bb6bc2d58334bf52475fdd7cad934f28079
Matteo De Wint <matteo@gorilla.co>
2023-08-05 00:35:28 +0200

    neovim: add lua lsp

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -12,4 +12,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 vim.loader.enable()
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -50,6 +50,25 @@ return {
       },
     })
 
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    })
+
     local servers = {
       "bashls",
       "rust_analyzer",
