fea44eac0cc1cd0e325c1a3a29c43d9472d32880
Matteo De Wint <matteo@gorilla.co>
2023-08-05 19:47:57 +0200

    neovim: use wildmenu for command completion

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -11,13 +11,11 @@ end
 
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp",
-    -- "hrsh7th/cmp-nvim-lsp-signature-help",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
   },
@@ -29,7 +27,6 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
-        -- { name = "nvim_lsp_signature_help" },
         { name = "buffer" },
       }),
       snippet = {
@@ -71,6 +68,9 @@ return {
       sources = {
         { name = "buffer" },
       },
+      view = {
+        entries = { name = "wildmenu" },
+      },
     })
 
     cmp.setup.cmdline(":", {
@@ -79,6 +79,9 @@ return {
         { name = "cmdline" },
         { name = "path" },
       }),
+      view = {
+        entries = { name = "wildmenu" },
+      },
     })
   end,
 }
