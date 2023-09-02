e769d5f22c578a03c132605c2c19dd94205c8d7a
Matteo De Wint <matteo@gorilla.co>
2023-09-02 23:28:56 +0200

    neovim: remove comment keymaps

--- neovim/.config/nvim/lua/plugins/comment.lua
+++ neovim/.config/nvim/lua/plugins/comment.lua
@@ -1,8 +1,5 @@
 return {
   "numToStr/Comment.nvim",
-  config = function()
-    require("Comment").setup()
-    vim.api.nvim_set_keymap("n", "<c-_>", "gcc", {})
-    vim.api.nvim_set_keymap("v", "<c-_>", "gccgv", {})
-  end,
+  opts = {},
+  lazy = false,
 }
