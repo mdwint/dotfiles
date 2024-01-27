b3a8caf1e6c6a6f1aae6beef88f93ddd685e5bd8
Matteo De Wint <matteo@gorilla.co>
2024-01-27 22:37:58 +0100

    neovim: add obsidian plugin

---
+++ neovim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,27 @@
+local vault = vim.fn.expand("~")
+  .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes"
+
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  event = {
+    "BufReadPre " .. vault .. "/**.md",
+    "BufNewFile " .. vault .. "/**.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "notes",
+        path = vault,
+      },
+    },
+    disable_frontmatter = true,
+    ui = {
+      enable = false,
+    },
+  },
+}
