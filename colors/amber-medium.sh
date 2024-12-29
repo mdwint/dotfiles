865eff5eaf1a7cae08e647a831267b0c65068604
Matteo De Wint <matteo@gorilla.co>
2024-12-29 23:35:55 +0100

    colors: add amber-dark and amber-medium

---
+++ colors/amber-medium.sh
@@ -0,0 +1,16 @@
+color00=30/11/06
+color01=9f/1c/20
+color02=ce/df/73
+color03=f2/95/20
+color04=ef/3e/23
+color05=ff/00/00
+color06=ea/56/27
+color07=ef/43/23
+color08=54/0b/01
+color15=ff/af/97
+color16=ff/d5/99
+color17=b4/1f/24
+color18=38/16/11
+color19=43/1a/13
+color20=$color07
+color21=$color15
