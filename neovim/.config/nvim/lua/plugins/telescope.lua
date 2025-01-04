852bedccbcda231ff8d6fc5534602fc8549c3a53
Matteo De Wint <matteo@gorilla.co>
2025-01-04 16:56:22 +0100

    neovim: move telescope keymaps to lazy

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,15 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-telescope/telescope-ui-select.nvim",
   },
+  keys = {
+    { "<c-p>", ":Telescope find_files hidden=true theme=ivy<cr>" },
+    { "<leader>a", ":Telescope live_grep theme=ivy<cr>" },
+    { "<leader>8", ":Telescope grep_string theme=ivy<cr>" },
+    { "<leader>r", ":Telescope lsp_references theme=ivy<cr>" },
+    { "<leader>b", ":Telescope buffers theme=ivy<cr>" },
+    { "<leader>h", ":Telescope help_tags theme=ivy<cr>" },
+    { "z=", ":Telescope spell_suggest theme=cursor<cr>" },
+  },
   config = function()
     local actions = require("telescope.actions")
     local themes = require("telescope.themes")
@@ -32,17 +41,5 @@ return {
 
     require("telescope").load_extension("fzf")
     require("telescope").load_extension("ui-select")
-
-    vim.cmd([[
-    nnoremap <c-p> :Telescope find_files hidden=true theme=ivy<cr>
-    nnoremap <leader>a :Telescope live_grep theme=ivy<cr>
-    nnoremap <leader>8 :Telescope grep_string theme=ivy<cr>
-    nnoremap <leader>r :Telescope lsp_references theme=ivy<cr>
-    nnoremap <leader>b :Telescope buffers theme=ivy<cr>
-    nnoremap <leader>h :Telescope help_tags theme=ivy<cr>
-    nnoremap <leader>l :Telescope loclist theme=ivy<cr>
-    nnoremap <leader>m :Telescope keymaps theme=ivy<cr>
-    nnoremap z= :Telescope spell_suggest theme=cursor<cr>
-    ]])
   end,
 }
