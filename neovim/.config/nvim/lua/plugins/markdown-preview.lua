0100539be217ee5be1efe95012d999cf05fd5f5f
Matteo De Wint <matteo@mailfence.com>
2025-07-07 20:09:48 +0200

    neovim: remove markdown-preview plugin

--- neovim/.config/nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-local ft = { "markdown", "changelogmd" }
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
-  init = function() vim.g.mkdp_filetypes = ft end,
-  ft = ft,
-}
