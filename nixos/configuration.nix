821e566889024aa3d7a06ff0ea49cdb0a04a858d
Matteo De Wint <matteo@mailfence.com>
2025-08-03 16:09:37 +0200

    nix: disable blueman

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -29,7 +29,7 @@
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
+  # services.blueman.enable = true;
 
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
