e249b32dae8fb1bedba84ccab11f8ae96ff1c83f
Matteo De Wint <matteo@mailfence.com>
2025-08-09 18:35:40 +0200

    nix: mount disks on raspberrypi

--- nix/raspberrypi/configuration.nix
+++ nix/raspberrypi/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -6,6 +6,8 @@
     ../shared/system/user.nix
   ];
 
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+  boot.initrd.availableKernelModules = [ "usb_storage" ];
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
@@ -14,6 +16,31 @@
 
   time.timeZone = "Europe/Brussels";
   i18n.defaultLocale = "en_US.UTF-8";
+
+  fileSystems = {
+    "/" = lib.mkForce {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+
+    "/mnt/blue" = {
+      device = "/dev/disk/by-label/Blue";
+      fsType = "ext4";
+      options = [ "noatime" "nofail" "x-systemd.device-timeout=10" ];
+    };
+
+    "/mnt/red" = {
+      device = "/dev/disk/by-label/Red";
+      fsType = "ext4";
+      options = [ "noatime" "nofail" "x-systemd.device-timeout=10" ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /mnt/blue 0755 root root -"
+    "d /mnt/red  0755 root root -"
+  ];
 
   environment.systemPackages = with pkgs; [
   ];
