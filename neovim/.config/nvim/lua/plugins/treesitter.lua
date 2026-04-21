799f8a22493f1d779e0d79e4b6f1e53d4300fa98
Matteo De Wint <matteo@mailfence.com>
2026-04-21 17:23:37 +0200

    neovim: update treesitter config

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-context",
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({ ---@diagnostic disable-line
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        "css",
-        "dockerfile",
-        "fish",
-        "go",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "rst",
-        "rust",
-        "terraform",
-        "toml",
-        "vim",
-        "yaml",
-        "zig",
-      },
-      highlight = {
-        enable = true,
-        -- additional_vim_regex_highlighting = { "markdown" },
-      },
-    })
-  end,
 }
