cb727213a190a9ac2f1f2a98c98a340e60d6b802
Matteo De Wint <matteo@mailfence.com>
2026-09-17 09:07:14 +0200

    nix: configure gc and store optimisation

--- nix/shared/system/base.nix
+++ nix/shared/system/base.nix
@@ -3,5 +3,11 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nixpkgs.config.allowUnfree = true;
 
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 30d";
+  };
+  nix.optimise.automatic = true;
+
   programs.fish.enable = true;
 }
